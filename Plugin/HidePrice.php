fa4d73f8637894e95a0b8109b9006ba9e81f0d14
Magecan <wuqun2005@hotmail.com>
2025-03-31 17:11:16 -0400

    Add files via upload

---
+++ Plugin/HidePrice.php
@@ -0,0 +1,69 @@
+<?php
+namespace Magecan\HidePrice\Plugin;
+
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Customer\Model\Session;
+use Magento\Store\Model\ScopeInterface;
+ 
+class HidePrice
+{
+    protected $customerSession;
+    protected $scopeConfig;
+    protected $shouldHidePrice = null;
+
+    public function __construct(
+        Session $customerSession,
+		ScopeConfigInterface $scopeConfig
+    ) {
+        $this->customerSession = $customerSession;
+        $this->scopeConfig = $scopeConfig;
+    }
+
+    /**
+     * Modify the HTML output after the block renders
+     *
+     * @param \Magento\Framework\Pricing\Render\PriceBox
+     * @param string $result
+     * @return string
+     */
+    public function afterToHtml(\Magento\Framework\Pricing\Render\PriceBox $subject, $result)
+    {
+		if (!$this->getShouldHidePrice()) {
+			return $result;
+		}
+
+        return $this->getConfigValue('hide_price/general/hide_price_text');
+    }
+
+    private function getShouldHidePrice()
+    {
+		if (!is_bool($this->shouldHidePrice)) {
+			if (!$this->getConfigValue('hide_price/general/enabled')) {
+				$this->shouldHidePrice = false;
+				return $this->shouldHidePrice;
+			}
+
+			if ($this->customerSession->isLoggedIn()) {
+				$customerGroupId = $this->customerSession->getCustomer()->getGroupId();
+			} else {
+				$customerGroupId = \Magento\Customer\Model\GroupManagement::NOT_LOGGED_IN_ID;
+			}
+
+			$customerGroups = $this->getConfigValue('hide_price/general/customer_groups');
+			$customerGroups = $customerGroups ? explode(',', $customerGroups) : [];
+	 
+			$this->shouldHidePrice = in_array($customerGroupId, $customerGroups);			
+		}
+		
+		return $this->shouldHidePrice;
+	}
+
+	private function getConfigValue($path, $storeId = null)
+	{
+		return $this->scopeConfig->getValue(
+			$path,
+			ScopeInterface::SCOPE_STORE,
+			$storeId
+		);
+	}
+}
